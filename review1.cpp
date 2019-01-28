7f012ddc1d80ecafe00f89fd358fddf8832a52d3
Ryan <ryan.shim@gmail.com>
2019-01-28 09:19:49 -0800

    removed ans

--- review1.cpp
+++ review1.cpp
@@ -13,36 +13,26 @@ int get_power(int base, int exponent) {
 }
 
 void print_array(int *arr, int size) {
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
+    // TO BE COMPLETED
 }
 
 int main() {
     const int size = 10;
 
     // Declare an array of size 10 in the stack.
-    int foo[size];
 
     // Populate the values of the array declared above with
     // the powers of 2 using the function provided above.
-    for (int i = 0; i < size; ++i) {
-        foo[i] = get_power(2, i);
-    }
+
 
     // Declare an array of size 10 in the heap.
-    int* bar = new int[size]; 
+
 
     // Populate the values of the array declared in the heap
     // with the powers of 3 using the function provided above.
-    for (int i = 0; i < size; ++i) {
-        bar[i] = get_power(3, i);
-    }
+
 
     // Print out both arrays.
-    print_array(foo, size);
-    print_array(bar, size);
 
     return 0;
 }
